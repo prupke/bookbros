e05cfc48e98b2cdab31578f0f5594e7b3ae0c53b
Paul Rupke <paul.rupke@gmail.com>
2019-02-19 12:06:13 +0100

    Fixed syntax in books.rb

--- app/controllers/books.rb
+++ app/controllers/books.rb
@@ -1,10 +1,12 @@
-books = {
-	in_the_skin_of_a_lion {
-		title: 'In the Skin of a Lion', 
-		author: 'Michael Ondaatje'
+books = [
+	{
+		id: "in_the_skin_of_a_lion",
+		name: "In the Skin of a Lion", 
+		author: "Michael Ondaatje"
 	},
-	strangers_on_a_train {
-		title: 'Strangers on a Train',
-		author: 'Patricia Highsmith'
+	{
+		id: "strangers_on_a_train",
+		name: "Strangers on a Train", 
+		author: "Patricia Highsmith"
 	}
-}
+]

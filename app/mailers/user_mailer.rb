0f4819b1ec1b49f859f1d137b0e444d7cc5f7bbc
Paul Rupke <paul.rupke@gmail.com>
2019-04-17 21:29:17 -0400

    Edited mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,8 @@ class UserMailer < ApplicationMailer
 	default from: 'noreply@bookbros.club'
 
 	def register_email
-	    @user = params[:user]
 	    @email = params[:email]
-	    @link = params[:link]
+	    @club_link = params[:club_link]
 	    # @url  = 'http://example.com/login'
 	    mail(to: @email, subject: 'Welcome to Book Bros!')		
 	end

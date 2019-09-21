508e8232cd0b87bbabd79bc328294712e4db4eae
Paul Rupke <paul.rupke@gmail.com>
2019-09-21 19:52:23 -0400

    Changed posts index - added else

--- app/models/club.rb
+++ app/models/club.rb
@@ -6,6 +6,6 @@ class Club < ApplicationRecord
 	# has_many :users, dependent: :destroy
 	# accepts_nested_attributes_for :users
 	validates :name, presence: true, length: { minimum: 3 }	
-	validates :email, presence: true, length: { minimum: 7 }   
+	# validates :email, presence: true, length: { minimum: 7 }   
 	# validates :user_attributes, presence: true
 end
